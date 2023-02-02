d92587772a0be6c3f7a24d535794a63ef5cafc87
Jesús Rojas <jrojash@jys.pe>
2023-02-02 11:54:29 -0500

    Squashed commit of the following:
    commit 75b04cbd5dc92330c56641e8e79d8551eaa273e1
    Author: Jesús Rojas <jrojash@jys.pe>
    Date:   Thu Feb 2 11:52:05 2023 -0500
    
        update
    
    commit 5051f5ff1a72c4b67e7586cff8c9613527ef55fd
    Author: Jesús Rojas <jrojash@jys.pe>
    Date:   Thu Feb 2 10:23:03 2023 -0500
    
        Create composer.json

---
+++ classes/APP.php
@@ -0,0 +1,22 @@
+<?php
+/*!
+ * classes/APP.php
+ * @filesource
+ */
+defined('APPPATH') or exit(0); ## Acceso directo no autorizado
+
+/**
+ * APP
+ * Núcleo del sistema
+ */
+class APP
+{
+	/**
+	 * process()
+	 * @static
+	 */
+	public static function process ()
+	{
+		
+	}
+}
